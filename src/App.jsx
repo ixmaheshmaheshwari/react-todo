79aab8ddb770b370c2fee278ecc15b603920c38d
ixmaheshmaheshwari <157010602+ixmaheshmaheshwari@users.noreply.github.com>
2024-02-13 11:35:35 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Todo from './Todo'
+import Todo from './components/Todo'
 
 function App() {
   const [count, setCount] = useState(0)
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,33 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './App.css'; // Import your CSS file for styling
+import '../components/Todo.css'; 
 import ReactPaginate from 'react-paginate';
 
 const baseURL = "https://jsonplaceholder.typicode.com/todos";
-const todosPerPage = 5;
 
+const todosPerPage = 5;
+const authToken = 'e8248e17-b168-49a1-b655-af30b079a160'
 const Todo = () => {
   const [todos, setTodos] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
-  const [newId,setNewId]=useState(200)
-
+  const [newId,setNewId]=useState(201)
+  const getData=()=>{
+    // axios.get(`${baseURL}`)
+    // axios.get(`${baseURL}?_page=${currentPage}&_limit=${todosPerPage}`)
+    // .then((res) => {
+    //   setTodos(res.data);
+    //   const totalCount = Number(res.headers["x-total-count"]);
+    //   setTotalPages(Math.ceil(totalCount / todosPerPage));
+    // })
+    // .catch((error) => {
+    //   console.error('Error fetching todos', error);
+    // });
+   
+axios.get(`https://apis-production-145a.up.railway.app/api/todo/`,{
+    headers: {
+      Authorization: `e8248e17-b168-49a1-b655-af30b079a160`
+   }
+  })
+  .then((res) => {
+    // setTodos(res.data);
+    console.log(res)
+    // const totalCount = Number(res.headers["x-total-count"]);
+  // setTotalPages(Math.ceil(totalCount / todosPerPage));
+  })
+  .catch((error) => {
+    console.error('Error fetching todos', error);
+  });
+  }
   useEffect(() => {
-    axios.get(`${baseURL}?_page=${currentPage}&_limit=${todosPerPage}`)
-      .then((res) => {
-        setTodos(res.data);
-        const totalCount = Number(res.headers["x-total-count"]);
-        setTotalPages(Math.ceil(totalCount / todosPerPage));
-      })
-      .catch((error) => {
-        console.error('Error fetching todos', error);
-      });
+    getData();
   }, [currentPage]);
 
   const handleDelete = (id) => {
     axios.delete(`${baseURL}/${id}`)
       .then(() => {
-        setTodos(todos.filter(todo => todo.id !== id));
+        // setTodos(todos.filter(todo => todo.id !== id));
+        getData();
+
       })
       .catch((error) => {
         console.error('Error deleting todo', error);
@@ -35,9 +56,11 @@ const Todo = () => {
   };
 
   const handleUpdate = (id, updatedTitle) => {
-    axios.put('https://jsonplaceholder.typicode.com/todos/{id}', { title: updatedTitle })
+    axios.put(`${baseURL}/${id}`, { title: updatedTitle })
       .then(() => {
-        setTodos(todos.map(todo => todo.id === id ? { ...todo, title: updatedTitle } : todo));
+        // setTodos(todos.map(todo => todo.id === id ? { ...todo, title: updatedTitle } : todo));
+        getData();
+
       })
       .catch((error) => {
         console.error('Error updating todo', error);
@@ -45,10 +68,18 @@ const Todo = () => {
   };
 
   const handleCreate = (newTodo) => {
-    axios.post(baseURL, { title: newTodo, completed: false,id:newId })
+    const updatedId = newId + 1;
+    axios.post(baseURL, { title: newTodo, completed: false, id: updatedId, userId: 201 },{
+      headers: {
+        Authorization: `e8248e17-b168-49a1-b655-af30b079a160`
+     }
+    })
       .then((res) => {
-        setTodos([...todos, res.data]);
-        setNewId(prev=>prev+1);
+        // setTodos([...todos, res.data]);
+        getData();
+
+        setNewId(updatedId);
+        console.log("newId:"+newId);
       })
       .catch((error) => {
         console.error('Error creating todo', error);
@@ -66,7 +97,7 @@ const Todo = () => {
     <div className="todo-container">
       <div className="todo-content">
         <h3 className="todo-header">Incomplete Tasks</h3>
-        {incompleteTodos.map((todo) => (
+        { incompleteTodos.map((todo) => (
           <div key={todo.id} className="todo-card">
             <div className="todo-card-header">
               {todo.title}
